22e10691293ca6c393223e24d76ce49fc1e358d0
Marc Nadim Zeenny <marcnzeenny@gmail.com>
2025-02-13 01:30:09 +0200

    Skl and advs form + url correction

--- app.py
+++ app.py
@@ -9,39 +9,44 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/about-us')
+@app.route('/about-us/')
 def about_us():
     return render_template('about-us.html')
 
 
-@app.route('/team')
+@app.route('/team/')
 def team():
     return render_template('team.html')
 
 
-@app.route('/gallery')
+@app.route('/gallery/')
 def gallery():
     return render_template('gallery.html')
 
 
-@app.route('/beymun-2025')
+@app.route('/beymun-2025/')
 def beymun_2025():
     return render_template('beymun-2025.html')
 
 
-@app.route('/register')
+@app.route('/register/')
 def register():
     return render_template('register.html')
 
 
-@app.route('/delegate-regist')
+@app.route('/delegate-regist/')
 def delegate_regist():
     return redirect('https://docs.google.com/forms/d/e/1FAIpQLSdw7V8QvQmZ-pVZW2zyuvWoMo4nEOqNKR-2_-iDH_0RnFdiYA/viewform?usp=dialog')
 
 
-@app.route('/dais-regist')
+@app.route('/dais-regist/')
 def dias_regist():
     return redirect('https://docs.google.com/forms/d/e/1FAIpQLScSyGvmngW0M-B3l_GpQzizg5a2woA5t8pRWaP9OAg4pI37Qg/viewform?usp=header')
+
+
+@app.route('/skl-regist/')
+def skl_regist():
+    return redirect('https://docs.google.com/forms/d/e/1FAIpQLSeQehJsIliEddOHRXlAaDBJ9CPgn_ha5zXxM_-N9vNcjbmXAg/viewform?usp=header')
 
 
 if __name__ == '__main__':
